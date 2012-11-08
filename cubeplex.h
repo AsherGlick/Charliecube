1295d6a9d21dd38d608cfe91eada87c1ae37c76b
Spencer Bliven <spencer.bliven@gmail.com>
2012-11-08 00:16:28 -0800

    Update cubeplex.h to use the pin values from mapping.h.
    Added some more comments.

--- cubeplex.h
+++ cubeplex.h
@@ -26,19 +26,24 @@
 #ifndef _CUBEPLEX_H_
 #define _CUBEPLEX_H_
 
+// number of LEDs
 #define BUFFERSIZE 192
 #include "WProgram.h"
 #include "mappings.h"
 #include "niceTimer.h"
 
+// Node for a linked list of the pins which turn on an LED
 struct _frame_light{
   char pin1;
   char pin2;
   struct _frame_light * next;
 };
 
+// Beginning of linked list of all LEDs
 _frame_light * _cube__frame;
+// Currently lit LED
 _frame_light * _cube_current_frame;
+// array with state of each LED
 char * _cube_buffer;
 
 bool continuePattern = false;
@@ -87,7 +92,7 @@ void clearBuffer () {
 
 /**************************** NEXT COLOR FUNCTIONS ****************************\
 | These functions cycle thorugh                                                |
-|  the three primary colors red, greed, and blue                               |
+|  the three primary colors red, green, and blue                               |
 |  the secondary colors purple, teal, yellow (every pairing of two leds)       |
 |  the ALL THE COLORS (red,green,blue,purple,teal,yellow,white)                |
 \******************************************************************************/
@@ -105,9 +110,9 @@ void swapint(int & one, int & two) {
   one = one^two;
 }
 
-/******************************* ROUND CLOSTEST *******************************\
+/******************************* ROUND CLOSEST *******************************\
 | This function takes in a numberator and denominator and rounds to the        |
-| nearist number instead of trunkating. It does this by calculating an extra   |
+| nearist number instead of truncating. It does this by calculating an extra   |
 | digit (this function should be changes to be more accurate)                  |
 \******************************************************************************/
 int roundClostest(int numerator, int denominator) {
@@ -326,202 +331,22 @@ void flushBuffer() {
   _frame_light * copy_frame = _cube__frame;
   display_length = 0;
   
-  
-  //TODO the pins values need to be changed so that the mapping.h file can
-  // change them using #defines
-  if (_cube_buffer[  0] != 0)flushElement(copy_frame, 4, 8,_cube_buffer[  0]);
-  if (_cube_buffer[  1] != 0)flushElement(copy_frame,16, 4,_cube_buffer[  1]);
-  if (_cube_buffer[  2] != 0)flushElement(copy_frame,12,16,_cube_buffer[  2]);
-  if (_cube_buffer[  3] != 0)flushElement(copy_frame, 8,12,_cube_buffer[  3]);
-  if (_cube_buffer[  4] != 0)flushElement(copy_frame, 4, 7,_cube_buffer[  4]);
-  if (_cube_buffer[  5] != 0)flushElement(copy_frame,13, 4,_cube_buffer[  5]);
-  if (_cube_buffer[  6] != 0)flushElement(copy_frame,11,13,_cube_buffer[  6]);
-  if (_cube_buffer[  7] != 0)flushElement(copy_frame, 7,11,_cube_buffer[  7]);
-  if (_cube_buffer[  8] != 0)flushElement(copy_frame, 4, 6,_cube_buffer[  8]);
-  if (_cube_buffer[  9] != 0)flushElement(copy_frame,15, 4,_cube_buffer[  9]);
-  if (_cube_buffer[ 10] != 0)flushElement(copy_frame,10,15,_cube_buffer[ 10]);
-  if (_cube_buffer[ 11] != 0)flushElement(copy_frame, 6,10,_cube_buffer[ 11]);
-  if (_cube_buffer[ 12] != 0)flushElement(copy_frame, 4, 5,_cube_buffer[ 12]);
-  if (_cube_buffer[ 13] != 0)flushElement(copy_frame,14, 4,_cube_buffer[ 13]);
-  if (_cube_buffer[ 14] != 0)flushElement(copy_frame, 9,14,_cube_buffer[ 14]);
-  if (_cube_buffer[ 15] != 0)flushElement(copy_frame, 5, 9,_cube_buffer[ 15]);
-  if (_cube_buffer[ 16] != 0)flushElement(copy_frame, 3, 8,_cube_buffer[ 16]);
-  if (_cube_buffer[ 17] != 0)flushElement(copy_frame,15, 3,_cube_buffer[ 17]);
-  if (_cube_buffer[ 18] != 0)flushElement(copy_frame,11,15,_cube_buffer[ 18]);
-  if (_cube_buffer[ 19] != 0)flushElement(copy_frame, 8,11,_cube_buffer[ 19]);
-  if (_cube_buffer[ 20] != 0)flushElement(copy_frame, 3, 7,_cube_buffer[ 20]);
-  if (_cube_buffer[ 21] != 0)flushElement(copy_frame,14, 3,_cube_buffer[ 21]);
-  if (_cube_buffer[ 22] != 0)flushElement(copy_frame,12,14,_cube_buffer[ 22]);
-  if (_cube_buffer[ 23] != 0)flushElement(copy_frame, 7,12,_cube_buffer[ 23]);
-  if (_cube_buffer[ 24] != 0)flushElement(copy_frame, 3, 6,_cube_buffer[ 24]);
-  if (_cube_buffer[ 25] != 0)flushElement(copy_frame,16, 3,_cube_buffer[ 25]);
-  if (_cube_buffer[ 26] != 0)flushElement(copy_frame, 9,16,_cube_buffer[ 26]);
-  if (_cube_buffer[ 27] != 0)flushElement(copy_frame, 6, 9,_cube_buffer[ 27]);
-  if (_cube_buffer[ 28] != 0)flushElement(copy_frame, 3, 5,_cube_buffer[ 28]);
-  if (_cube_buffer[ 29] != 0)flushElement(copy_frame,13, 3,_cube_buffer[ 29]);
-  if (_cube_buffer[ 30] != 0)flushElement(copy_frame,10,13,_cube_buffer[ 30]);
-  if (_cube_buffer[ 31] != 0)flushElement(copy_frame, 5,10,_cube_buffer[ 31]);
-  if (_cube_buffer[ 32] != 0)flushElement(copy_frame, 2, 8,_cube_buffer[ 32]);
-  if (_cube_buffer[ 33] != 0)flushElement(copy_frame,14, 2,_cube_buffer[ 33]);
-  if (_cube_buffer[ 34] != 0)flushElement(copy_frame,10,14,_cube_buffer[ 34]);
-  if (_cube_buffer[ 35] != 0)flushElement(copy_frame, 8,10,_cube_buffer[ 35]);
-  if (_cube_buffer[ 36] != 0)flushElement(copy_frame, 2, 7,_cube_buffer[ 36]);
-  if (_cube_buffer[ 37] != 0)flushElement(copy_frame,15, 2,_cube_buffer[ 37]);
-  if (_cube_buffer[ 38] != 0)flushElement(copy_frame, 9,15,_cube_buffer[ 38]);
-  if (_cube_buffer[ 39] != 0)flushElement(copy_frame, 7, 9,_cube_buffer[ 39]);
-  if (_cube_buffer[ 40] != 0)flushElement(copy_frame, 2, 6,_cube_buffer[ 40]);
-  if (_cube_buffer[ 41] != 0)flushElement(copy_frame,13, 2,_cube_buffer[ 41]);
-  if (_cube_buffer[ 42] != 0)flushElement(copy_frame,12,13,_cube_buffer[ 42]);
-  if (_cube_buffer[ 43] != 0)flushElement(copy_frame, 6,12,_cube_buffer[ 43]);
-  if (_cube_buffer[ 44] != 0)flushElement(copy_frame, 2, 5,_cube_buffer[ 44]);
-  if (_cube_buffer[ 45] != 0)flushElement(copy_frame,16, 2,_cube_buffer[ 45]);
-  if (_cube_buffer[ 46] != 0)flushElement(copy_frame,11,16,_cube_buffer[ 46]);
-  if (_cube_buffer[ 47] != 0)flushElement(copy_frame, 5,11,_cube_buffer[ 47]);
-  if (_cube_buffer[ 48] != 0)flushElement(copy_frame, 1, 8,_cube_buffer[ 48]);
-  if (_cube_buffer[ 49] != 0)flushElement(copy_frame,13, 1,_cube_buffer[ 49]);
-  if (_cube_buffer[ 50] != 0)flushElement(copy_frame, 9,13,_cube_buffer[ 50]);
-  if (_cube_buffer[ 51] != 0)flushElement(copy_frame, 8, 9,_cube_buffer[ 51]);
-  if (_cube_buffer[ 52] != 0)flushElement(copy_frame, 1, 7,_cube_buffer[ 52]);
-  if (_cube_buffer[ 53] != 0)flushElement(copy_frame,16, 1,_cube_buffer[ 53]);
-  if (_cube_buffer[ 54] != 0)flushElement(copy_frame,10,16,_cube_buffer[ 54]);
-  if (_cube_buffer[ 55] != 0)flushElement(copy_frame, 7,10,_cube_buffer[ 55]);
-  if (_cube_buffer[ 56] != 0)flushElement(copy_frame, 1, 6,_cube_buffer[ 56]);
-  if (_cube_buffer[ 57] != 0)flushElement(copy_frame,14, 1,_cube_buffer[ 57]);
-  if (_cube_buffer[ 58] != 0)flushElement(copy_frame,11,14,_cube_buffer[ 58]);
-  if (_cube_buffer[ 59] != 0)flushElement(copy_frame, 6,11,_cube_buffer[ 59]);
-  if (_cube_buffer[ 60] != 0)flushElement(copy_frame, 1, 5,_cube_buffer[ 60]);
-  if (_cube_buffer[ 61] != 0)flushElement(copy_frame,15, 1,_cube_buffer[ 61]);
-  if (_cube_buffer[ 62] != 0)flushElement(copy_frame,12,15,_cube_buffer[ 62]);
-  if (_cube_buffer[ 63] != 0)flushElement(copy_frame, 5,12,_cube_buffer[ 63]);
-  if (_cube_buffer[ 64] != 0)flushElement(copy_frame,16, 8,_cube_buffer[ 64]);
-  if (_cube_buffer[ 65] != 0)flushElement(copy_frame,12, 4,_cube_buffer[ 65]);
-  if (_cube_buffer[ 66] != 0)flushElement(copy_frame, 8,16,_cube_buffer[ 66]);
-  if (_cube_buffer[ 67] != 0)flushElement(copy_frame, 4,12,_cube_buffer[ 67]);
-  if (_cube_buffer[ 68] != 0)flushElement(copy_frame,13, 7,_cube_buffer[ 68]);
-  if (_cube_buffer[ 69] != 0)flushElement(copy_frame,11, 4,_cube_buffer[ 69]);
-  if (_cube_buffer[ 70] != 0)flushElement(copy_frame, 7,13,_cube_buffer[ 70]);
-  if (_cube_buffer[ 71] != 0)flushElement(copy_frame, 4,11,_cube_buffer[ 71]);
-  if (_cube_buffer[ 72] != 0)flushElement(copy_frame,15, 6,_cube_buffer[ 72]);
-  if (_cube_buffer[ 73] != 0)flushElement(copy_frame,10, 4,_cube_buffer[ 73]);
-  if (_cube_buffer[ 74] != 0)flushElement(copy_frame, 6,15,_cube_buffer[ 74]);
-  if (_cube_buffer[ 75] != 0)flushElement(copy_frame, 4,10,_cube_buffer[ 75]);
-  if (_cube_buffer[ 76] != 0)flushElement(copy_frame,14, 5,_cube_buffer[ 76]);
-  if (_cube_buffer[ 77] != 0)flushElement(copy_frame, 9, 4,_cube_buffer[ 77]);
-  if (_cube_buffer[ 78] != 0)flushElement(copy_frame, 5,14,_cube_buffer[ 78]);
-  if (_cube_buffer[ 79] != 0)flushElement(copy_frame, 4, 9,_cube_buffer[ 79]);
-  if (_cube_buffer[ 80] != 0)flushElement(copy_frame,15, 8,_cube_buffer[ 80]);
-  if (_cube_buffer[ 81] != 0)flushElement(copy_frame,11, 3,_cube_buffer[ 81]);
-  if (_cube_buffer[ 82] != 0)flushElement(copy_frame, 8,15,_cube_buffer[ 82]);
-  if (_cube_buffer[ 83] != 0)flushElement(copy_frame, 3,11,_cube_buffer[ 83]);
-  if (_cube_buffer[ 84] != 0)flushElement(copy_frame,14, 7,_cube_buffer[ 84]);
-  if (_cube_buffer[ 85] != 0)flushElement(copy_frame,12, 3,_cube_buffer[ 85]);
-  if (_cube_buffer[ 86] != 0)flushElement(copy_frame, 7,14,_cube_buffer[ 86]);
-  if (_cube_buffer[ 87] != 0)flushElement(copy_frame, 3,12,_cube_buffer[ 87]);
-  if (_cube_buffer[ 88] != 0)flushElement(copy_frame,16, 6,_cube_buffer[ 88]);
-  if (_cube_buffer[ 89] != 0)flushElement(copy_frame, 9, 3,_cube_buffer[ 89]);
-  if (_cube_buffer[ 90] != 0)flushElement(copy_frame, 6,16,_cube_buffer[ 90]);
-  if (_cube_buffer[ 91] != 0)flushElement(copy_frame, 3, 9,_cube_buffer[ 91]);
-  if (_cube_buffer[ 92] != 0)flushElement(copy_frame,13, 5,_cube_buffer[ 92]);
-  if (_cube_buffer[ 93] != 0)flushElement(copy_frame,10, 3,_cube_buffer[ 93]);
-  if (_cube_buffer[ 94] != 0)flushElement(copy_frame, 5,13,_cube_buffer[ 94]);
-  if (_cube_buffer[ 95] != 0)flushElement(copy_frame, 3,10,_cube_buffer[ 95]);
-  if (_cube_buffer[ 96] != 0)flushElement(copy_frame,14, 8,_cube_buffer[ 96]);
-  if (_cube_buffer[ 97] != 0)flushElement(copy_frame,10, 2,_cube_buffer[ 97]);
-  if (_cube_buffer[ 98] != 0)flushElement(copy_frame, 8,14,_cube_buffer[ 98]);
-  if (_cube_buffer[ 99] != 0)flushElement(copy_frame, 2,10,_cube_buffer[ 99]);
-  if (_cube_buffer[100] != 0)flushElement(copy_frame,15, 7,_cube_buffer[100]);
-  if (_cube_buffer[101] != 0)flushElement(copy_frame, 9, 2,_cube_buffer[101]);
-  if (_cube_buffer[102] != 0)flushElement(copy_frame, 7,15,_cube_buffer[102]);
-  if (_cube_buffer[103] != 0)flushElement(copy_frame, 2, 9,_cube_buffer[103]);
-  if (_cube_buffer[104] != 0)flushElement(copy_frame,13, 6,_cube_buffer[104]);
-  if (_cube_buffer[105] != 0)flushElement(copy_frame,12, 2,_cube_buffer[105]);
-  if (_cube_buffer[106] != 0)flushElement(copy_frame, 6,13,_cube_buffer[106]);
-  if (_cube_buffer[107] != 0)flushElement(copy_frame, 2,12,_cube_buffer[107]);
-  if (_cube_buffer[108] != 0)flushElement(copy_frame,16, 5,_cube_buffer[108]);
-  if (_cube_buffer[109] != 0)flushElement(copy_frame,11, 2,_cube_buffer[109]);
-  if (_cube_buffer[110] != 0)flushElement(copy_frame, 5,16,_cube_buffer[110]);
-  if (_cube_buffer[111] != 0)flushElement(copy_frame, 2,11,_cube_buffer[111]);
-  if (_cube_buffer[112] != 0)flushElement(copy_frame,13, 8,_cube_buffer[112]);
-  if (_cube_buffer[113] != 0)flushElement(copy_frame, 9, 1,_cube_buffer[113]);
-  if (_cube_buffer[114] != 0)flushElement(copy_frame, 8,13,_cube_buffer[114]);
-  if (_cube_buffer[115] != 0)flushElement(copy_frame, 1, 9,_cube_buffer[115]);
-  if (_cube_buffer[116] != 0)flushElement(copy_frame,16, 7,_cube_buffer[116]);
-  if (_cube_buffer[117] != 0)flushElement(copy_frame,10, 1,_cube_buffer[117]);
-  if (_cube_buffer[118] != 0)flushElement(copy_frame, 7,16,_cube_buffer[118]);
-  if (_cube_buffer[119] != 0)flushElement(copy_frame, 1,10,_cube_buffer[119]);
-  if (_cube_buffer[120] != 0)flushElement(copy_frame,14, 6,_cube_buffer[120]);
-  if (_cube_buffer[121] != 0)flushElement(copy_frame,11, 1,_cube_buffer[121]);
-  if (_cube_buffer[122] != 0)flushElement(copy_frame, 6,14,_cube_buffer[122]);
-  if (_cube_buffer[123] != 0)flushElement(copy_frame, 1,11,_cube_buffer[123]);
-  if (_cube_buffer[124] != 0)flushElement(copy_frame,15, 5,_cube_buffer[124]);
-  if (_cube_buffer[125] != 0)flushElement(copy_frame,12, 1,_cube_buffer[125]);
-  if (_cube_buffer[126] != 0)flushElement(copy_frame, 5,15,_cube_buffer[126]);
-  if (_cube_buffer[127] != 0)flushElement(copy_frame, 1,12,_cube_buffer[127]);
-  if (_cube_buffer[128] != 0)flushElement(copy_frame,12, 8,_cube_buffer[128]);
-  if (_cube_buffer[129] != 0)flushElement(copy_frame, 8, 4,_cube_buffer[129]);
-  if (_cube_buffer[130] != 0)flushElement(copy_frame, 4,16,_cube_buffer[130]);
-  if (_cube_buffer[131] != 0)flushElement(copy_frame,16,12,_cube_buffer[131]);
-  if (_cube_buffer[132] != 0)flushElement(copy_frame,11, 7,_cube_buffer[132]);
-  if (_cube_buffer[133] != 0)flushElement(copy_frame, 7, 4,_cube_buffer[133]);
-  if (_cube_buffer[134] != 0)flushElement(copy_frame, 4,13,_cube_buffer[134]);
-  if (_cube_buffer[135] != 0)flushElement(copy_frame,13,11,_cube_buffer[135]);
-  if (_cube_buffer[136] != 0)flushElement(copy_frame,10, 6,_cube_buffer[136]);
-  if (_cube_buffer[137] != 0)flushElement(copy_frame, 6, 4,_cube_buffer[137]);
-  if (_cube_buffer[138] != 0)flushElement(copy_frame, 4,15,_cube_buffer[138]);
-  if (_cube_buffer[139] != 0)flushElement(copy_frame,15,10,_cube_buffer[139]);
-  if (_cube_buffer[140] != 0)flushElement(copy_frame, 9, 5,_cube_buffer[140]);
-  if (_cube_buffer[141] != 0)flushElement(copy_frame, 5, 4,_cube_buffer[141]);
-  if (_cube_buffer[142] != 0)flushElement(copy_frame, 4,14,_cube_buffer[142]);
-  if (_cube_buffer[143] != 0)flushElement(copy_frame,14, 9,_cube_buffer[143]);
-  if (_cube_buffer[144] != 0)flushElement(copy_frame,11, 8,_cube_buffer[144]);
-  if (_cube_buffer[145] != 0)flushElement(copy_frame, 8, 3,_cube_buffer[145]);
-  if (_cube_buffer[146] != 0)flushElement(copy_frame, 3,15,_cube_buffer[146]);
-  if (_cube_buffer[147] != 0)flushElement(copy_frame,15,11,_cube_buffer[147]);
-  if (_cube_buffer[148] != 0)flushElement(copy_frame,12, 7,_cube_buffer[148]);
-  if (_cube_buffer[149] != 0)flushElement(copy_frame, 7, 3,_cube_buffer[149]);
-  if (_cube_buffer[150] != 0)flushElement(copy_frame, 3,14,_cube_buffer[150]);
-  if (_cube_buffer[151] != 0)flushElement(copy_frame,14,12,_cube_buffer[151]);
-  if (_cube_buffer[152] != 0)flushElement(copy_frame, 9, 6,_cube_buffer[152]);
-  if (_cube_buffer[153] != 0)flushElement(copy_frame, 6, 3,_cube_buffer[153]);
-  if (_cube_buffer[154] != 0)flushElement(copy_frame, 3,16,_cube_buffer[154]);
-  if (_cube_buffer[155] != 0)flushElement(copy_frame,16, 9,_cube_buffer[155]);
-  if (_cube_buffer[156] != 0)flushElement(copy_frame,10, 5,_cube_buffer[156]);
-  if (_cube_buffer[157] != 0)flushElement(copy_frame, 5, 3,_cube_buffer[157]);
-  if (_cube_buffer[158] != 0)flushElement(copy_frame, 3,13,_cube_buffer[158]);
-  if (_cube_buffer[159] != 0)flushElement(copy_frame,13,10,_cube_buffer[159]);
-  if (_cube_buffer[160] != 0)flushElement(copy_frame,10, 8,_cube_buffer[160]);
-  if (_cube_buffer[161] != 0)flushElement(copy_frame, 8, 2,_cube_buffer[161]);
-  if (_cube_buffer[162] != 0)flushElement(copy_frame, 2,14,_cube_buffer[162]);
-  if (_cube_buffer[163] != 0)flushElement(copy_frame,14,10,_cube_buffer[163]);
-  if (_cube_buffer[164] != 0)flushElement(copy_frame, 9, 7,_cube_buffer[164]);
-  if (_cube_buffer[165] != 0)flushElement(copy_frame, 7, 2,_cube_buffer[165]);
-  if (_cube_buffer[166] != 0)flushElement(copy_frame, 2,15,_cube_buffer[166]);
-  if (_cube_buffer[167] != 0)flushElement(copy_frame,15, 9,_cube_buffer[167]);
-  if (_cube_buffer[168] != 0)flushElement(copy_frame,12, 6,_cube_buffer[168]);
-  if (_cube_buffer[169] != 0)flushElement(copy_frame, 6, 2,_cube_buffer[169]);
-  if (_cube_buffer[170] != 0)flushElement(copy_frame, 2,13,_cube_buffer[170]);
-  if (_cube_buffer[171] != 0)flushElement(copy_frame,13,12,_cube_buffer[171]);
-  if (_cube_buffer[172] != 0)flushElement(copy_frame,11, 5,_cube_buffer[172]);
-  if (_cube_buffer[173] != 0)flushElement(copy_frame, 5, 2,_cube_buffer[173]);
-  if (_cube_buffer[174] != 0)flushElement(copy_frame, 2,16,_cube_buffer[174]);
-  if (_cube_buffer[175] != 0)flushElement(copy_frame,16,11,_cube_buffer[175]);
-  if (_cube_buffer[176] != 0)flushElement(copy_frame, 9, 8,_cube_buffer[176]);
-  if (_cube_buffer[177] != 0)flushElement(copy_frame, 8, 1,_cube_buffer[177]);
-  if (_cube_buffer[178] != 0)flushElement(copy_frame, 1,13,_cube_buffer[178]);
-  if (_cube_buffer[179] != 0)flushElement(copy_frame,13, 9,_cube_buffer[179]);
-  if (_cube_buffer[180] != 0)flushElement(copy_frame,10, 7,_cube_buffer[180]);
-  if (_cube_buffer[181] != 0)flushElement(copy_frame, 7, 1,_cube_buffer[181]);
-  if (_cube_buffer[182] != 0)flushElement(copy_frame, 1,16,_cube_buffer[182]);
-  if (_cube_buffer[183] != 0)flushElement(copy_frame,16,10,_cube_buffer[183]);
-  if (_cube_buffer[184] != 0)flushElement(copy_frame,11, 6,_cube_buffer[184]);
-  if (_cube_buffer[185] != 0)flushElement(copy_frame, 6, 1,_cube_buffer[185]);
-  if (_cube_buffer[186] != 0)flushElement(copy_frame, 1,14,_cube_buffer[186]);
-  if (_cube_buffer[187] != 0)flushElement(copy_frame,14,11,_cube_buffer[187]);
-  if (_cube_buffer[188] != 0)flushElement(copy_frame,12, 5,_cube_buffer[188]);
-  if (_cube_buffer[189] != 0)flushElement(copy_frame, 5, 1,_cube_buffer[189]);
-  if (_cube_buffer[190] != 0)flushElement(copy_frame, 1,15,_cube_buffer[190]);
-  if (_cube_buffer[191] != 0)flushElement(copy_frame,15,12,_cube_buffer[191]);
+  int i=0;  
+  // Do some macro manipulation to build up flushElement calls for all LEDs
+  // Expands to 192 lines of the FLUSH_ELEM macro, with the pins replaced by the mappings in mappins.h:
+  // b111,b112,b113,b114,b121,b122,...,g111,...,r444
+#define FLUSH_ELEM(pins) if(_cube_buffer[ i ] != 0) flushElement(copy_frame, pins ,_cube_buffer[ i ]); i++
+#define PINS_Z(cxy) FLUSH_ELEM( cxy##1 ); FLUSH_ELEM( cxy##2 ); FLUSH_ELEM( cxy##3 ); FLUSH_ELEM( cxy##4 )
+#define PINS_Y(cx) PINS_Z(cx##1); PINS_Z(cx##2); PINS_Z(cx##3); PINS_Z(cx##4)
+#define PINS_X(c) PINS_Y(c##1); PINS_Y(c##2); PINS_Y(c##3); PINS_Y(c##4);
 
+  PINS_X(b); PINS_X(g); PINS_X(r);
+
+#undef PINS_X
+#undef PINS_Y
+#undef PINS_Z
+#undef FLUSH_ELEM
+  
   (_cube__frame+1)->next=copy_frame;
   _cube_current_frame=_cube__frame+1;
 }
--- mappings.h
+++ mappings.h
@@ -2,75 +2,83 @@
 #define _MAPPINGS_H_
 
 /****************************** PIN PORT MAPPINGS *****************************\
-|
+| There are 16 logical groups for controlling the LEDs. This defines which     |
+| physical pin coresponds to each. For example, to use analogue pin 2 (PORTC   |
+| bit 3) for group 15, we set P13C=0x04 and P13B=P13D=0;                       |
+|                                                                              |
+| See ATmega328 specs for more info about direct port access.                  |
 \******************************************************************************/
 #define P1B 0x00
 #define P1C 0x00
-#define P1D 0x04
+#define P1D 0x04 //2
 
 #define P2B 0x00
 #define P2C 0x00
-#define P2D 0x08
+#define P2D 0x08//3
 
 #define P3B 0x00
 #define P3C 0x00
-#define P3D 0x10
+#define P3D 0x10//4
 
 #define P4B 0x00
 #define P4C 0x00
-#define P4D 0x20
+#define P4D 0x20//5
 
 #define P5B 0x00
 #define P5C 0x00
-#define P5D 0x40
+#define P5D 0x40//6
 
 #define P6B 0x00
 #define P6C 0x00
-#define P6D 0x80
+#define P6D 0x80//7
 
-#define P7B 0x01
+#define P7B 0x01//8
 #define P7C 0x00
 #define P7D 0x00
 
-#define P8B 0x02
+#define P8B 0x02//9
 #define P8C 0x00
 #define P8D 0x00
 
-#define P9B 0x04
+#define P9B 0x04//10
 #define P9C 0x00
 #define P9D 0x00
 
-#define P10B 0x08
+#define P10B 0x08//11
 #define P10C 0x00
 #define P10D 0x00
 
-#define P11B 0x10
+#define P11B 0x10//12
 #define P11C 0x00
 #define P11D 0x00
 
-#define P12B 0x20
+#define P12B 0x20//13
 #define P12C 0x00
 #define P12D 0x00
 
 #define P13B 0x00
-#define P13C 0x01
+#define P13C 0x01//A0
 #define P13D 0x00
 
 #define P14B 0x00
-#define P14C 0x02
+#define P14C 0x02//A1
 #define P14D 0x00
 
 #define P15B 0x00
-#define P15C 0x04
+#define P15C 0x04//A2
 #define P15D 0x00
 
 #define P16B 0x00
-#define P16C 0x08
+#define P16C 0x08//A3
 #define P16D 0x00
 
 
-/*
-// LED PIN MAPPINGS
+
+/* LED group mappings
+ *
+ * This defines which group to turn on for each LED. The first group specifies the cathode, the second the anode.
+ * The pins are named based on color, x, y, z.
+ */
 #define b111 4,8
 #define b112 16,4
 #define b113 12,16
@@ -264,199 +272,5 @@
 #define r443 1,15
 #define r444 15,12
 
-// LED PIN Positions
-#define b111 4,8
-#define b112 16,4
-#define b113 12,16
-#define b114 8,12
-#define b121 4,7
-#define b122 13,4
-#define b123 11,13
-#define b124 7,11
-#define b131 4,6
-#define b132 15,4
-#define b133 10,15
-#define b134 6,10
-#define b141 4,5
-#define b142 14,4
-#define b143 9,14
-#define b144 5,9
-#define b211 3,8
-#define b212 15,3
-#define b213 11,15
-#define b214 8,11
-#define b221 3,7
-#define b222 14,3
-#define b223 12,14
-#define b224 7,12
-#define b231 3,6
-#define b232 16,3
-#define b233 9,16
-#define b234 6,9
-#define b241 3,5
-#define b242 13,3
-#define b243 10,13
-#define b244 5,10
-#define b311 2,8
-#define b312 14,2
-#define b313 10,14
-#define b314 8,10
-#define b321 2,7
-#define b322 15,2
-#define b323 9,15
-#define b324 7,9
-#define b331 2,6
-#define b332 13,2
-#define b333 12,13
-#define b334 6,12
-#define b341 2,5
-#define b342 16,2
-#define b343 11,16
-#define b344 5,11
-#define b411 1,8
-#define b412 13,1
-#define b413 9,13
-#define b414 8,9
-#define b421 1,7
-#define b422 16,1
-#define b423 10,16
-#define b424 7,10
-#define b431 1,6
-#define b432 14,1
-#define b433 11,14
-#define b434 6,11
-#define b441 1,5
-#define b442 15,1
-#define b443 12,15
-#define b444 5,12
-#define g111 16,8
-#define g112 12,4
-#define g113 8,16
-#define g114 4,12
-#define g121 13,7
-#define g122 11,4
-#define g123 7,13
-#define g124 4,11
-#define g131 15,6
-#define g132 10,4
-#define g133 6,15
-#define g134 4,10
-#define g141 14,5
-#define g142 9,4
-#define g143 5,14
-#define g144 4,9
-#define g211 15,8
-#define g212 11,3
-#define g213 8,15
-#define g214 3,11
-#define g221 14,7
-#define g222 12,3
-#define g223 7,14
-#define g224 3,12
-#define g231 16,6
-#define g232 9,3
-#define g233 6,16
-#define g234 3,9
-#define g241 13,5
-#define g242 10,3
-#define g243 5,13
-#define g244 3,10
-#define g311 14,8
-#define g312 10,2
-#define g313 8,14
-#define g314 2,10
-#define g321 15,7
-#define g322 9,2
-#define g323 7,15
-#define g324 2,9
-#define g331 13,6
-#define g332 12,2
-#define g333 6,13
-#define g334 2,12
-#define g341 16,5
-#define g342 11,2
-#define g343 5,16
-#define g344 2,11
-#define g411 13,8
-#define g412 9,1
-#define g413 8,13
-#define g414 1,9
-#define g421 16,7
-#define g422 10,1
-#define g423 7,16
-#define g424 1,10
-#define g431 14,6
-#define g432 11,1
-#define g433 6,14
-#define g434 1,11
-#define g441 15,5
-#define g442 12,1
-#define g443 5,15
-#define g444 1,12
-#define r111 12,8
-#define r112 8,4
-#define r113 4,16
-#define r114 16,12
-#define r121 11,7
-#define r122 7,4
-#define r123 4,13
-#define r124 13,11
-#define r131 10,6
-#define r132 6,4
-#define r133 4,15
-#define r134 15,10
-#define r141 9,5
-#define r142 5,4
-#define r143 4,14
-#define r144 14,9
-#define r211 11,8
-#define r212 8,3
-#define r213 3,15
-#define r214 15,11
-#define r221 12,7
-#define r222 7,3
-#define r223 3,14
-#define r224 14,12
-#define r231 9,6
-#define r232 6,3
-#define r233 3,16
-#define r234 16,9
-#define r241 10,5
-#define r242 5,3
-#define r243 3,13
-#define r244 13,10
-#define r311 10,8
-#define r312 8,2
-#define r313 2,14
-#define r314 14,10
-#define r321 9,7
-#define r322 7,2
-#define r323 2,15
-#define r324 15,9
-#define r331 12,6
-#define r332 6,2
-#define r333 2,13
-#define r334 13,12
-#define r341 11,5
-#define r342 5,2
-#define r343 2,16
-#define r344 16,11
-#define r411 9,8
-#define r412 8,1
-#define r413 1,13
-#define r414 13,9
-#define r421 10,7
-#define r422 7,1
-#define r423 1,16
-#define r424 16,10
-#define r431 11,6
-#define r432 6,1
-#define r433 1,14
-#define r434 14,11
-#define r441 12,5
-#define r442 5,1
-#define r443 1,15
-#define r444 15,12
-*/
 
 #endif
